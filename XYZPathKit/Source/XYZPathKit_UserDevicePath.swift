ae9d7d119b911859574b0c42d804a33a3a1efa35
zhangqian <zhangqianbrandy2012@gmail.com>
2020-04-07 17:49:17 +0800

    0.0.2

---
+++ XYZPathKit/Source/XYZPathKit_UserDevicePath.swift
@@ -0,0 +1,13 @@
+//
+//  XYZPathKit_UserDevicePath.swift
+//  XYZPathKit
+//
+//  Created by 张子豪 on 2020/4/7.
+//  Copyright © 2020 张子豪. All rights reserved.
+//
+
+import UIKit
+
+class XYZPathKit_UserDevicePath: NSObject {
+
+}
