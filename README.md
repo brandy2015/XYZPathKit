902b04e52d7c0b772dd2c231c8265d1392ce28e5
zhangqian <zhangqianbrandy2012@gmail.com>
2020-04-08 10:48:22 +0800

    2.0.2

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Easy way in Swift Path Managing
 * Xcode 11.4 or higher
 * iOS 13.0 or higher (may work on previous versions, just did not test it)
 * ARC
-* Swift 5.0
+* Swift 5.3
 
 ## Demo
 
--- XYZPathKit/Source/XYZPath_UIImage.swift
+++ XYZPathKit/Source/XYZPath_UIImage.swift
@@ -321,3 +321,26 @@ public extension UIImage{
 
 
 //旋转图片
+
+
+public extension UIImage {
+    /**
+     *  重设图片大小
+     */
+    func reSizeImage(reSize:CGSize)->UIImage {
+        //UIGraphicsBeginImageContext(reSize);
+        UIGraphicsBeginImageContextWithOptions(reSize,false,UIScreen.main.scale);
+        let 尺寸 = CGRect(x: 0, y: 0, width: reSize.width, height: reSize.height)
+        self.draw(in: 尺寸);
+        let reSizeImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!;
+        UIGraphicsEndImageContext();
+        return reSizeImage;
+    }
+    /**
+     *  等比率缩放
+     */
+    func scaleImage(scaleSize:CGFloat)->UIImage {
+        let reSize = CGSize(width: self.size.width * scaleSize, height: self.size.height * scaleSize)
+        return reSizeImage(reSize: reSize)
+    }
+}
